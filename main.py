d4f38b0559917d8b99058a57da7d37392177589e
henrique-rodrigues-sharkcoders <149248625+henrique-rodrigues-sharkcoders@users.noreply.github.com>
2023-10-28 15:08:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from math import sqrt
+
 while True:
     print("1-soma")
     print("2-subtração")
@@ -6,8 +7,10 @@ while True:
     print("4-divisão")
     print("5-potenciação")
     print("6-resto")
-    print("7-sair")
-     op = int(input("Escolha a operação que deseja: "))
+    print("7-sqrt")
+    print("8-sair")
+
+    op = int(input("Escolha a operação que deseja: "))
 
     if op == 1:
         n1 = int(input("Escolhe o primeiro número: "))
@@ -34,6 +37,10 @@ while True:
         n2 = int(input("Escolhe o segundo número: "))
         print(n1 % n2)
     elif op == 7:
+        n1 = int(input("Escolhe o primeiro número: "))
+        n2 = int(input("Escolhe o segundo número: "))
+        print(n1 sqrt n2)
+    elif op == 8:
         exit()
     else:
         print("operação inválida.")
